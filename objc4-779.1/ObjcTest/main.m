d41b948065bf8c706eb1e259f6898957d20f1384
Lemon <573986558@qq.com>
2020-03-23 16:13:42 +0800

    添加最新macOS 10.15下objc4-779.1

---
+++ objc4-779.1/ObjcTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ObjcTest
+//
+//  Created by neotv on 2020/3/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSObject * obj = [NSObject alloc];
+        
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
