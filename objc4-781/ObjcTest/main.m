7748801a493a4aba74d0df7677a55e665d1a86f6
hjw <573986558@qq.com>
2020-09-07 14:00:51 +0800

    update objc-781

---
+++ objc4-781/ObjcTest/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  ObjcTest
+//
+//  Created by neotv on 2020/9/7.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
+
+@implementation Person
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        NSObject *obj = [NSObject alloc];
+    
+        Person *person = [Person alloc];
+    }
+    return 0;
+}
