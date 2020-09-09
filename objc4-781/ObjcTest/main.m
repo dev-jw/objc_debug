2dde6384e17bbfe6d0665bbeb9c95a3bb1620b3e
hjw <573986558@qq.com>
2020-09-09 20:17:56 +0800

    update objc-781

--- objc4-781/ObjcTest/main.m
+++ objc4-781/ObjcTest/main.m
@@ -6,8 +6,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+#import <malloc/malloc.h>
 
 @interface Person : NSObject
+//@property (nonatomic, strong) NSString *name;
+//@property (nonatomic, strong) NSString *nickName;
+//
+//@property (nonatomic, assign) char a;
+//@property (nonatomic, assign) char b;
+//
+//@property (nonatomic, assign) int age;
+//@property (nonatomic, assign) int height;
 
 @end
 
@@ -15,14 +25,38 @@
 
 @end
 
+struct Year {
+    int year;
+}Year;
+
+struct YearWithMonth {
+    int year;
+    int month;
+}YearWithMonth;
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
         
         NSObject *obj = [NSObject alloc];
-    
+//
         Person *person = [Person alloc];
+        size_t pSize = sizeof(person);
+        size_t gSize  = class_getInstanceSize(Person.class);
+        size_t mSize = malloc_size((__bridge const void *)(person));
+        
+        struct Year y;
+        y.year = 2020;
+        size_t yearSize = sizeof(y);
+        
+        struct YearWithMonth ym;
+        ym.year  = 2020;
+        ym.month = 9;
+        size_t yearWithMonthSize = sizeof(ym);
+        
+        NSLog(@"Hello, World!");
+
     }
     return 0;
 }
