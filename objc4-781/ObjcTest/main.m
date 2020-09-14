e6aaf393f510c81ab2c069f05cf0712c2970fa73
HJW <573986558@qq.com>
2020-09-14 22:48:51 +0800

    update objc

--- objc4-781/ObjcTest/main.m
+++ objc4-781/ObjcTest/main.m
@@ -34,17 +34,42 @@ struct YearWithMonth {
     int month;
 }YearWithMonth;
 
+void validClass() {
+    Class class1 = [Person class];
+    Class class2 = [Person alloc].class;
+    Class class3 = object_getClass([Person alloc]);
+    NSLog(@"\n%p\n%p\n%p\n",class1,class2,class3);
+}
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         
+        BOOL res1 = [(id)[NSObject class] isKindOfClass:[NSObject class]];
+        BOOL res2 = [(id)[NSObject class] isMemberOfClass:[NSObject class]];
+        BOOL res3 = [(id)[Person class] isKindOfClass:[Person class]];
+        BOOL res4 = [(id)[Person class] isMemberOfClass:[Person class]];
+
+        NSLog(@"%d %d %d %d", res1, res2, res3, res4);
+        
+        BOOL res5 = [(id)[NSObject alloc] isKindOfClass:[NSObject class]];
+        BOOL res6 = [(id)[NSObject alloc] isMemberOfClass:[NSObject class]];
+        BOOL res7 = [(id)[Person alloc] isKindOfClass:[Person class]];
+        BOOL res8 = [(id)[Person alloc] isMemberOfClass:[Person class]];
+
+        NSLog(@"%d %d %d %d", res5, res6, res7, res8);
+        
+        validClass();
+        
         NSObject *obj = [NSObject alloc];
-//
+        //
         Person *person = [Person alloc];
         size_t pSize = sizeof(person);
         size_t gSize  = class_getInstanceSize(Person.class);
         size_t mSize = malloc_size((__bridge const void *)(person));
+        
+        
         
         struct Year y;
         y.year = 2020;
@@ -56,7 +81,7 @@ int main(int argc, const char * argv[]) {
         size_t yearWithMonthSize = sizeof(ym);
         
         NSLog(@"Hello, World!");
-
+        
     }
     return 0;
 }
